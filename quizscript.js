6dc4aed8ddd4c4bdfdd1517c2f48c8e0f50c8f93
heymonserratg <heymonserratg@gmail.com>
2022-12-12 16:29:07 -0500

    remote add practice

---
+++ quizscript.js
@@ -0,0 +1,11 @@
+code quiz
+
+click the start button
+timer starts with a question
+I answer a question then another question
+answer a question incorrectly time is subtracted from the clock
+game is over when questions are 
+answered/timer reaches 0
+
+WHEN the game is over, 
+save initials and score.
---
+++ scheduler.js
@@ -0,0 +1,29 @@
+
+daily planner:
+
+create a schedule
+
+open the planner, 
+
+current day is displayed  top of the calendar
+
+scroll down time blocks 
+
+for standard business hours
+
+view the time blocks 
+
+each time block is color-coded 
+
+indicate: past, present, or future
+
+click time block, enter an event
+
+save button for that time block
+
+text for that event is saved in 
+
+local storage
+
+refresh the page ,saved events show
+
